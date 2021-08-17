16c28ce575c065479ab1c3fae489a5067f6b808b
geomin76 <geom@vt.edu>
2021-08-17 11:33:55 -0400

    Adding data

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 import { parks } from './Parks';
 import { useState } from 'react';
+import { Checkbox, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 
 function App() {
 
   const [data, setData] = useState(parks);
+  
+  const setVisited = (index) => {
+    setData(prev => [...prev, prev[index].visited = true])
+    console.log(`${data[index].name} : ${data[index].visited}`)
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        
+        <List>
+          {data.map((value, index) => {
+            return (
+              <ListItem key={index} onClick={() => setVisited(index)}>
+                <ListItemIcon>
+                  <Checkbox checked={value.visited}/>
+                  <ListItemText primary={`${value.name}`}/>
+                </ListItemIcon>
+              </ListItem>
+            )
+          })}
+        </List>
       </header>
     </div>
   );
