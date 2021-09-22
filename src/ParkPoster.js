abfd2e5f99af17edc9025f2ff0fabe8f2f49de73
Geo Min <31418725+geomin76@users.noreply.github.com>
2021-09-22 10:58:05 -0400

    Adding clickable images and b&w (#6)
    * Adding images
    
    * clicking works
    
    * Adding grayscale

---
+++ src/ParkPoster.js
@@ -0,0 +1,82 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import { Image } from 'react-bootstrap';
+import {BrowserView, MobileView} from 'react-device-detect';
+var _ = require('lodash');
+
+export const ParkPoster = ({ data, setData }) => {
+
+    const setVisited = (name) => {
+        const index = data.findIndex((element, index) => {
+            if (element.name === name) {
+                return true
+            }
+        })
+        let newData = [...data];
+        newData[index].visited = !data[index].visited;
+        setData(newData);
+        console.log(`${data[index].name} : ${data[index].visited}`)
+    }
+
+    const renderBrowserView = () => {
+        const rows = _.chunk(data, 9);
+        return (
+            <>
+            {
+                rows.map((row, index1) => {
+                    return (
+                        <Row>
+                            {
+                                row.map((col, index2) => {
+                                    return (
+                                        <Col xl lg md><Image className="p-1" style={{ filter: col.visited ? "" : "grayscale(100%)" }} onClick={() => setVisited(col.name)} src={col.url} width="100%"/></Col>
+                                    )
+                                    
+                                })
+                            }
+                        </Row>
+                    )
+                    
+                })
+            }
+            </>
+        )
+    }
+
+    const renderMobileView = () => {
+        const rows = _.chunk(data, 21);
+        return (
+            <>
+            {
+                rows.map((row) => {
+                    return (
+                        <Row>
+                            {
+                                row.map((col) => {
+                                    return (
+                                        <Col sm={4} xs={4}><Image className="p-1" style={{ filter: col.visited ? "" : "grayscale(100%)" }} onClick={() => setVisited(col.name)} src={col.url} width="100%"/></Col>
+                                    )
+                                    
+                                })
+                            }
+                        </Row>
+                    )
+                    
+                })
+            }
+            </>
+        )
+    }
+
+    return (
+        <>
+            <BrowserView>
+                {renderBrowserView()}
+            </BrowserView>
+            <MobileView>
+                {renderMobileView()}
+            </MobileView>
+        </>
+    )
+}
