9956ce02bfbbc6e6290131b5cfb9ec79fc5f54fc
geomin76 <geom@vt.edu>
2021-10-13 11:36:50 -0400

    download works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { parks } from './Parks';
-import { useState } from 'react';
+import React, { useState  } from 'react';
 import { Container, Box, Grid } from '@material-ui/core';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import { NPList } from './NPList';
 import { ParkPoster } from './ParkPoster';
+import html2canvas from "html2canvas";
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -16,10 +17,13 @@ const useStyles = makeStyles((theme) =>
   }),
 );
 
-function App() {
+const App = () => {
   const classes = useStyles();
 
   const [data, setData] = useState(parks);
+
+  const printRef = React.useRef();
+
 
   const countVisitedParks = () => {
     let visited = 0;
@@ -31,25 +35,56 @@ function App() {
     return visited;
   }
 
+  const handleDownloadImage = async () => {
+    const element = printRef.current
+    const canvas = await html2canvas(element, {
+      useCORS: true,
+      scale: 4
+    });
+ 
+    const data = canvas.toDataURL('image/png');
+    const link = document.createElement('a');
+ 
+    if (typeof link.download === 'string') {
+      link.href = data;
+      link.download = 'image.jpg';
+ 
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } else {
+      window.open(data);
+    }
+  };
+  
+
   return (
-    <Container maxWidth="lg">
-      <ParkPoster data={data} setData={setData} />
-      <br/>
-      <Box>
-        <Grid container>
-          <Grid item lg={12} md={12} sm={12}>
-            <div className={classes.center}>
-              <NPList data={data} setData={setData}/>
-            </div>
-            <div className={classes.center}>
-              <p>{countVisitedParks()}/{parks.length}</p>
-            </div>
+    <div ref={printRef}>
+      <Container maxWidth="lg">
+        <ParkPoster data={data} setData={setData} />
+        <br/>
+        <Box>
+          <Grid container>
+            <Grid item lg={12} md={12} sm={12}>
+              <div className={classes.center}>
+                <NPList data={data} setData={setData}/>
+              </div>
+              <div className={classes.center}>
+                <p>{countVisitedParks()}/{parks.length}</p>
+              </div>
+            </Grid>
           </Grid>
-        </Grid>
+        </Box>
 
-      </Box>
+        <button type="button" onClick={handleDownloadImage}>
+          Download as Image
+        </button>
+  
 
-    </Container>   
+
+      </Container> 
+    </div>
+      
   );
 }
 
