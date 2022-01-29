2a911035715aacdc9dab301b4ab3936ec837b36d
Geo Min <31418725+geomin76@users.noreply.github.com>
2022-01-29 11:25:45 -0500

    adding css (#11)

--- src/ParkPoster.js
+++ src/ParkPoster.js
@@ -1,8 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import { Image } from 'react-bootstrap';
 import img from "./crater-lake-national-park_250x.jpg";
+import './Poster.css'
 var _ = require('lodash');
 
 export const ParkPoster = ({ data, setData }) => {
@@ -19,34 +17,33 @@ export const ParkPoster = ({ data, setData }) => {
         console.log(`${data[index].name} : ${data[index].visited}`)
     }
 
-    const renderBrowserView = () => {
+    const renderView = () => {
         const rows = _.chunk(data, 7);
         return (
-            <>
-            {
-                rows.map((row, index1) => {
-                    return (
-                        <Row>
-                            {
-                                row.map((col, index2) => {
-                                    return (
-                                        <Col xl lg md sm xs><div onClick={() => setVisited(col.name)}><Image className="p-1" style={{ filter: col.visited ? "" : "grayscale(100%)" }}  src={img} width="100%"/></div></Col>
-                                    )
-                                    
-                                })
-                            }
-                        </Row>
-                    )
-                    
-                })
-            }
-            </>
+            <div className="poster">
+                {
+                    rows.map((row, index1) => {
+                        return (
+                            <div className="inner">
+                                {
+                                    row.map((col, index2) => {
+                                        return (
+                                            <div onClick={() => setVisited(col.name)}><img className="posterImage" style={{ filter: col.visited ? "" : "grayscale(100%)" }}  src={img}/></div>
+                                        )
+                                        
+                                    })
+                                }
+                            </div>
+                        )
+                    })
+                } 
+            </div>
         )
     }
 
     return (
         <>
-            {renderBrowserView()}
+            {renderView()}
         </>
     )
 }
