d7396322403168a81c5c54bbf0ab22dc937339d5
geomin76 <geom@vt.edu>
2023-02-18 10:52:10 -0600

    hm maybe

--- src/Map.js
+++ src/Map.js
@@ -5,18 +5,18 @@ export const Map = () => {
     return (
         <>
             <div style={{ display: "inline-block", position: "relative" }}>
-                <img src={NPSMap} width={"100%"} height={"50%"} />
+                <img src={NPSMap} width={"100%"} height={"50%"} style={{position: 'static'}}/>
                 {/* <svg viewBox="0 0 100 100" style={{ position: "absolute", top: "0.4%", left: "30%" }}>
                     <DeleteIcon width="5%" />
                 </svg>
                 <svg viewBox="0 0 100 100" style={{ position: "absolute", top: "23.4%", left: "30%" }}>
                     <DeleteIcon width="5%" />
                 </svg> */}
-                <svg viewBox="0 0 100 100" style={{ position: "absolute", top: "10em", left: "30em" }}>
+                <svg viewBox="0 0 100 100" style={{ position: "absolute", top: "23.4%", left: "30%" }}>
                     <DeleteIcon width="5%" />
                 </svg>
 
-                <svg viewBox="0 0 100 100" style={{ position: "absolute", top: "15em", left: "45em" }}>
+                <svg viewBox="0 0 100 100" style={{ position: "absolute", top: "23.4%", left: "20%"}}>
                     <DeleteIcon width="5%" />
                 </svg>
 
