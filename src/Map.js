fc57764dd595ea27c8f6d562e7b4caadf03270b2
geomin76 <geom@vt.edu>
2023-02-18 09:43:34 -0600

    keep this as website for now

--- src/Map.js
+++ src/Map.js
@@ -4,15 +4,15 @@ import DeleteIcon from '@mui/icons-material/Delete';
 export const Map = () => {
     return (
         <>
-            <div style={{display: "inline-block", position: "relative"}}>
+            <div style={{ display: "inline-block", position: "relative"}}>
                 <img src={NPSMap} width={"100%"} height={"50%"} />
-                <svg viewBox="0 0 100 100" style={{position: "absolute", top: "13.4%", left: "40%"}}>
+                <svg viewBox="0 0 100 100" style={{ position: "absolute", top: "0.4%", left: "30%" }}>
                     <DeleteIcon width="5%" />
                 </svg>
-                <svg viewBox="0 0 100 100" style={{position: "absolute", top: "13.4%", left: "50%"}}>
+                <svg viewBox="0 0 100 100" style={{ position: "absolute", top: "23.4%", left: "30%" }}>
                     <DeleteIcon width="5%" />
                 </svg>
-                <svg viewBox="0 0 100 100" style={{position: "absolute", top: "20.4%", left: "30%"}}>
+                <svg viewBox="0 0 100 100" style={{ position: "absolute", top: "20.4%", left: "30%" }}>
                     <DeleteIcon width="5%" />
                 </svg>
             </div>
