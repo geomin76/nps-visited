141636a25e31bd81da9ae99c22be1d16dfec5f83
Geo Min <31418725+geomin76@users.noreply.github.com>
2023-06-18 11:39:41 -0500

    Adding initial poster to see how it looks (#29)
    * notes
    
    * notes
    
    * Initial poster idea

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { NPList } from './NPList';
 import { Tab, Tabs } from '@mui/material';
 import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
 import ChecklistOutlinedIcon from '@mui/icons-material/ChecklistOutlined';
+import InsertPhotoOutlinedIcon from '@mui/icons-material/InsertPhotoOutlined';
+import { NPMap } from './NPMap';
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -60,6 +62,7 @@ const App = () => {
             <Tabs value={value} onChange={handleChange}>
               <Tab icon={<ChecklistOutlinedIcon />} {...a11yProps(0)} />
               <Tab icon={<MapOutlinedIcon />} {...a11yProps(1)} />
+              <Tab icon={<InsertPhotoOutlinedIcon />} {...a11yProps(2)} />
             </Tabs>
           </Grid>
         </Grid>
@@ -74,10 +77,41 @@ const App = () => {
         <TabPanel value={value} index={1}>
           <Grid container className={classes.center}>
             <Grid sm={12} className={classes.center}>
-              <p>
-                Instead of a full US-map, divide into sections (6 of US) and then allow user to select and click on each subregion, then pick national parks
-              </p>
+              <NPMap data={data} setData={setData} />
+            </Grid>
+          </Grid>
 
+          {/* <p>
+            Instead of a full US-map, divide into sections and then allow user to select and click on each subregion, then pick national parks
+          </p>
+          <p>
+            Follow this: https://www.freecodecamp.org/news/how-to-make-clickable-svg-map-html-css/
+            <br/>
+            and create clickable modal for each region: https://mui.com/material-ui/react-modal/
+            <br/>
+            then inside the modal, user can click national parks for each region and marks theme
+            <br/>
+            zoom function? maybe???
+            <br/>
+             have some kind of black dot, then if clicked, a tree or pin or something fun
+          </p>
+          <p>
+            Create regions based on this:
+            <br/>
+            https://morethanjustparks.com/wp-content/uploads/2021/10/national-parks-map-corrected2.jpg
+            <br/>
+            https://www.nps.gov/aboutus/images/NPS-Unified-Regions-Map.jpg?maxwidth=1300&maxheight=1300&autorotate=false
+            <br/>
+            https://www.researchgate.net/profile/Vincent-Santucci/publication/328761204/figure/fig3/AS:689936000888835@1541504969307/Map-showing-the-seven-regions-of-the-US-National-Park-Service.ppm
+          </p> */}
+
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          <Grid container className={classes.center}>
+            <Grid sm={12} className={classes.center}>
+              <div style={{ paddingTop: "10px" }}>
+                <img src="/npposter.jpg" width="100%" />
+              </div>
             </Grid>
           </Grid>
         </TabPanel>
